951076f3469088a1511d428b9e50579ca740374b
yanzhang li <yanzhanglee@outlook.com>
2024-06-14 17:28:43 +0100

    Creates a guess letter method code in the file.

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,9 @@ print(word_list)
 
 word = random.choice(word_list)
 print(word)
+
+guess = input("Please enter a letter: ")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
