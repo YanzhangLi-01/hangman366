443319102391f5b7bf5b0b13304269632ac924a7
yanzhang li <yanzhanglee@outlook.com>
2024-06-15 12:37:10 +0100

    Builds 2 methods, guess and input.

--- milestone_4.py
+++ milestone_4.py
@@ -7,7 +7,25 @@ class hangman:
         self.word = random.choice(self.word)
         self.word_guessed = ["_" for _ in word_list]
         self.num_letters = len(set(self.word))
-        self.list_of_guesses: []
-
+        self.list_of_guesses = []
+    
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word.")
+            self.num_lives -= 1
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter your guess: ")
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guess:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                guess.append(self.list_of_guess)
 
     
