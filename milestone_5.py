a57ad8bf0b0b95e0a2899323a72a4561980714fb
yanzhang li <yanzhanglee@outlook.com>
2024-06-15 13:26:47 +0100

    Creates play founction to run the game.

--- milestone_5.py
+++ milestone_5.py
@@ -14,8 +14,8 @@ class Hangman:
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             for i in range(len(self.word)):
-                if self.word[i] == guess:
-                    self.word_guessed[i] = guess 
+                if self.word[i-1] == guess:
+                    self.word_guessed[i-1] = guess 
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
@@ -26,8 +26,30 @@ class Hangman:
             guess = input("Please enter your guess: ")
             if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guess:
+            elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
-                guess.append(self.list_of_guess)
+                self.list_of_guesses.append(guess)
+                break
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    
+    while True:
+        if num_lives == 0:
+            print("You lost!")
+            break
+        if game.num_lives > 0:
+            game.ask_for_input()
+        if num_lives != 0 and game.num_letters <= 0:
+            print('Congratulations. You won the game!')
+            break
+
+
+if __name__ == "__main__":
+    word_list = ['apple', 'banana', 'cherry']
+    play_game(word_list)
+
+        
