8ee317f032f26ade84cb1b19d6e0180b9c75fb91
yanzhang li <yanzhanglee@outlook.com>
2024-06-15 13:04:48 +0100

    Replaces _ to guess word.

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = random.choice(self.word)
+        self.word = random.choice(word_list)
         self.word_guessed = ["_" for _ in word_list]
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
@@ -13,6 +13,9 @@ class hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i] = guess 
         else:
             print(f"Sorry, {guess} is not in the word.")
             self.num_lives -= 1
@@ -28,4 +31,7 @@ class hangman:
                 self.check_guess(guess)
                 guess.append(self.list_of_guess)
 
-    
+#if __name__ == "__main__":
+#    word_list = ['apple', 'banana', 'cherry']
+#    game = hangman(word_list)
+#    print(f"Word to guess: {game.word}")
