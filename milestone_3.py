d8569469b35cc509c5dc8001ae3b26268f1d1448
yanzhang li <yanzhanglee@outlook.com>
2024-06-15 11:09:48 +0100

    Creates 2 functions to make code cleaier.

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,19 @@
-while True:
-    guess = input("Please guess a letter among alphabet: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-print("You guess: ", guess)
-
 secret_word = ["apple", "strawberry", "cherry", "grape", "banana"]
-if guess in secret_word[0]:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again")
+
+def check_guess(guess):
+    guess = guess.lower()
+    if any(guess in word for word in secret_word):
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again")
+
+def ask_for_input():
+    while True:
+            guess = input("Please guess a letter among alphabet: ")
+            if len(guess) == 1 and guess.isalpha():
+                break
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
